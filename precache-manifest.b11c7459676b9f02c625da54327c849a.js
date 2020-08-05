1d94faca1e33b0bb59dcaa24e47b9d1def9a73f0
Vikash Kumar <vikashsmart1996@gmail.com>
2020-08-05 11:43:42 +0530

    Updates

---
+++ precache-manifest.b11c7459676b9f02c625da54327c849a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b014cf0df235e0cee0ff5e9b7e4d3a4c",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "31020fe5d192690fd6e0",
+    "url": "/covid-19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "d64ae5a20faabdc5f836",
+    "url": "/covid-19-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "31020fe5d192690fd6e0",
+    "url": "/covid-19-tracker/static/js/2.a5516439.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-19-tracker/static/js/2.a5516439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64ae5a20faabdc5f836",
+    "url": "/covid-19-tracker/static/js/main.b0851912.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
